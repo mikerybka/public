7772fddc31f6d0e53b4b3df196bc81953ed6f242
Michael Rybka <mikerybka@users.noreply.github.com>
2022-05-28 12:08:40 -0400

    Create examples.go

---
+++ pkg/services/examples.go
@@ -0,0 +1,14 @@
+package services
+
+type BuiltinExample string
+type BuiltinExample2 int
+
+type MapExample map[string]string
+
+type SliceExample []string
+
+type StructExample struct {
+  People []string
+  Places map[string]string
+  Things string
+}
